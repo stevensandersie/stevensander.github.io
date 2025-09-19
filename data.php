ddfa4d1805e119df6747e0f92566d152931a026d
stevensandersie <stevensander068@gmail.com>
2025-09-19 16:27:48 +0700

    1.2.8

--- data.php
+++ data.php
@@ -9,18 +9,18 @@
 return [
     // Informasi Pribadi untuk bagian Hero
     'personal_info' => [
-        'name' => 'Maria Firsty Anggarine',
-        'profile_picture_url' => 'https://static.wixstatic.com/media/1be581_3fc0873fdefd464ab2c6f199e9d21cab~mv2.jpg/v1/crop/x_64,y_0,w_1124,h_1359/fill/w_180,h_180,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/WhatsApp%20Image%202024-10-12%20at%2011_edited_j.jpg',
-        'description' => 'Saya adalah mahasiswi Business Information Technology di Universitas Bina Nusantara dengan passion kuat di bidang UI/UX design, business analysis, dan project management. Saya aktif dalam berbagai organisasi dan kompetisi untuk terus mengembangkan skill dan kontribusi pada proyek yang berdampak.',
-        'cv_url' => 'https://drive.google.com/file/d/1M0ChsB1qNDsooEoC2rPzApThGip4XDT4/view?usp=sharing',
+        'name' => 'Steven Sander',
+        'profile_picture_url' => 'https://cdn.momoi.cc/uploads/2025-09-19_b55b0153a6/2025-09-19_b7f981b454.jpg',
+        'description' => 'Saya adalah mahasiswa Business Information Technology di BINUS',
+        'cv_url' => 'linkcv',
         'contact_me_url' => '#contact'
     ],
 
     // Teks untuk bagian header di seksi Proyek
     'projects_section' => [
         'title' => 'Competitions & Projects',
-        'description' => 'Selama masa kuliah, saya telah berpartisipasi dalam berbagai kompetisi dan proyek yang berfokus pada UI/UX, analisis sistem, dan ide bisnis untuk mengaplikasikan ilmu secara praktis.',
-        'see_all_url' => 'https://firsstty.wixsite.com/firstyportofolio/blog'
+        'description' => 'link',
+        'see_all_url' => 'link'
     ],
 
     // Daftar Proyek Anda
@@ -28,20 +28,20 @@ return [
         [
             'title' => 'KataPintar: Aplikasi Pendukung Penderita Autisme',
             'category' => 'UI/UX Competition',
-            'image_url' => 'https://static.wixstatic.com/media/1be581_26818e5cb6a649c190778018e23f3d47~mv2.jpg/v1/fill/w_500,h_281,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/1be581_26818e5cb6a649c190778018e23f3d47~mv2.jpg',
-            'project_url' => 'https://firsstty.wixsite.com/firstyportofolio/post/katapintar-menginsipirasi-perubahan-dalam-meraih-masa-depan'
+            'image_url' => 'link',
+            'project_url' => 'link'
         ],
         [
-            'title' => 'Satu University: Platform Pencarian Beasiswa',
+            'title' => 'judul',
             'category' => 'UI/UX Competition',
-            'image_url' => 'https://static.wixstatic.com/media/1be581_79d5770db1fd49c8954303ff4dc81e07~mv2.jpg/v1/fill/w_500,h_281,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/1be581_79d5770db1fd49c8954303ff4dc81e07~mv2.jpg',
-            'project_url' => 'https://firsstty.wixsite.com/firstyportofolio/post/satu-university-find-your-best-suited-scholarship-right-here'
+            'image_url' => 'link',
+            'project_url' => 'link'
         ],
         [
-            'title' => 'SiagaLansia: Aplikasi Kesehatan untuk Lansia',
+            'title' => 'judul',
             'category' => 'UI/UX Competition',
-            'image_url' => 'https://static.wixstatic.com/media/1be581_ef0cf9d4bc234e9ab2719ca03cee726b~mv2.jpg/v1/fill/w_500,h_281,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/1be581_ef0cf9d4bc234e9ab2719ca03cee726b~mv2.jpg',
-            'project_url' => 'https://firsstty.wixsite.com/firstyportofolio/post/siagalansia-selalu-hadir-selalu-siaga'
+            'image_url' => 'link',
+            'project_url' => 'link'
         ]
     ],
     
@@ -53,12 +53,12 @@ return [
 
     // Daftar Skill Anda
     'skills' => [
-        ['name' => 'Figma', 'image_url' => 'https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg'],
-        ['name' => 'Canva', 'image_url' => 'https://upload.wikimedia.org/wikipedia/commons/0/08/Canva_icon_2021.svg'],
-        ['name' => 'Python', 'image_url' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png'],
-        ['name' => 'Ms. Word', 'image_url' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Microsoft_Office_Word_%282019%E2%80%93present%29.svg/1200px-Microsoft_Office_Word_%282019%E2%80%93present%29.svg.png'],
-        ['name' => 'Ms. Excel', 'image_url' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Microsoft_Office_Excel_%282019%E2%80%93present%29.svg/1200px-Microsoft_Office_Excel_%282019%E2%80%93present%29.svg.png'],
-        ['name' => 'Ms. PowerPoint', 'image_url' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Microsoft_Office_PowerPoint_%282019%E2%80%93present%29.svg/1200px-Microsoft_Office_PowerPoint_%282019%E2%80%93present%29.svg.png']
+        ['name' => 'Figma', 'image_url' => 'link'],
+        ['name' => 'Canva', 'image_url' => 'link'],
+        ['name' => 'Python', 'image_url' => 'link'],
+        ['name' => 'Ms. Word', 'image_url' => 'link'],
+        ['name' => 'Ms. Excel', 'image_url' => 'link'],
+        ['name' => 'Ms. PowerPoint', 'image_url' => 'link']
     ],
 
     // Informasi Kontak di Footer
@@ -66,9 +66,9 @@ return [
         'phone' => '0856-0751-5016',
         'email' => 'firsstty@gmail.com',
         'social_links' => [
-            'LinkedIn' => 'http://www.linkedin.com/in/mariafirstyanggarine',
-            'Instagram' => 'https://www.instagram.com/firsstty',
-            'GitHub' => 'https://github.com/yourusername' // Ganti dengan username GitHub Anda
+            'LinkedIn' => 'link',
+            'Instagram' => 'link',
+            'GitHub' => 'link' // Ganti dengan username GitHub Anda
         ]
     ]
 ];
