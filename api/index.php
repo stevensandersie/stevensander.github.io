2c75f61144f3c8e712803bb9b280d0925ee5046f
stevensandersie <stevensander068@gmail.com>
2025-09-20 08:40:21 +0700

    1.5.6

--- api/index.php
+++ api/index.php
@@ -9,8 +9,7 @@ $categories = ['All', 'Project', 'Education', 'Certificate', 'Activity'];
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= htmlspecialchars($data['personal_info']['name']) ?> | Portfolio</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title><?= htmlspecialchars($data['personal_info']['name']) ?> | Portfolio</title>
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
